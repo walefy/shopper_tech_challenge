5c6525cff8fdaddb5adf746dec26fff0cf2ef857
walefy <walefyd1@gmail.com>
2024-08-28 14:09:12 -0300

    test: add test to happy path of /confirm

--- tests/measure.integration.test.ts
+++ tests/measure.integration.test.ts
@@ -7,6 +7,7 @@ import { ImageService } from '../src/service/ImageService';
 import type { GeminiInterpretImageResponse } from '../src/types/GeminiInterpretImageResponse';
 import type { PutImageResponse } from '../src/types/PutImageResponse';
 import { restoreDb } from './scripts/restoreDb';
+import type { MeasureType } from '@prisma/client';
 
 describe('Integration test from Measure', () => {
   const sandbox = sinon.createSandbox();
@@ -27,23 +28,25 @@ describe('Integration test from Measure', () => {
 
   };
 
+  const createMeasure = async (customerCode: string, measureType: MeasureType, datetime: Date) => {
+    const data = {
+      image: '',
+      customer_code: customerCode,
+      measure_type: measureType,
+      measure_datetime: datetime
+    };
+    
+    return await request(app)
+    .post('/upload')
+    .send(data)
+    .set('Accept', 'application/json');
+  };
+
   test('Test that /upload works correctly with the correct data', async () => {  
     stubImageInterpret({ ok: true, payload: { value: 25 } })
     stubImageUpload({ ok: true, payload: { url: 'http://test.com' } });
 
-    const measure_datetime = new Date();
-    
-    const data = {
-      image: '',
-      customer_code: '2',
-      measure_type: 'GAS',
-      measure_datetime
-    };
-    const response = await request(app)
-    .post('/upload')
-    .send(data)
-    .set('Accept', 'application/json');
-
+    const response = await createMeasure('2', 'GAS', new Date());
     expect(response.status).toBe(200);
     expect(response.body).toHaveProperty('image_url');
     expect(response.body.image_url).toBe('http://test.com');
@@ -57,22 +60,10 @@ describe('Integration test from Measure', () => {
     stubImageInterpret({ ok: true, payload: { value: 25 } });
     stubImageUpload({ ok: true, payload: { url: 'http://test.com' } });
 
-    const data = {
-      image: '',
-      customer_code: '2',
-      measure_type: 'GAS',
-      measure_datetime: new Date(),
-    };
+    const datetime = new Date();
 
-    await request(app)
-    .post('/upload')
-    .send(data)
-    .set('Accept', 'application/json');
-
-    const response = await request(app)
-    .post('/upload')
-    .send(data)
-    .set('Accept', 'application/json');
+    await createMeasure('2', 'WATER', datetime);
+    const response = await createMeasure('2', 'WATER', datetime);
 
     expect(response.status).toBe(409);
     expect(response.body).toHaveProperty('error_code');
@@ -85,17 +76,7 @@ describe('Integration test from Measure', () => {
     stubImageInterpret({ ok: false, payload: { errorDescription: 'an error occurred in gemini, try again.' } });
     stubImageUpload({ ok: true, payload: { url: 'http://test.com' } });
 
-    const data = {
-      image: '',
-      customer_code: '2',
-      measure_type: 'GAS',
-      measure_datetime: new Date(),
-    };
-
-    const response = await request(app)
-    .post('/upload')
-    .send(data)
-    .set('Accept', 'application/json');
+    const response = await createMeasure('11223', 'GAS', new Date());
 
     expect(response.status).toBe(500);
     expect(response.body).toHaveProperty('error_code');
@@ -108,22 +89,33 @@ describe('Integration test from Measure', () => {
     stubImageInterpret({ ok: true, payload: { value: 25 } });
     stubImageUpload({ ok: false, payload: { errorDescription: 'image cant be uploaded!' } });
 
-    const data = {
-      image: '',
-      customer_code: '2',
-      measure_type: 'GAS',
-      measure_datetime: new Date(),
-    };
-
-    const response = await request(app)
-    .post('/upload')
-    .send(data)
-    .set('Accept', 'application/json');
+    const response = await createMeasure('11223', 'GAS', new Date());
 
     expect(response.status).toBe(500);
     expect(response.body).toHaveProperty('error_code');
     expect(response.body).toHaveProperty('error_description');
     expect(response.body.error_code).toBe('UPLOAD_IMAGE_ERROR');
     expect(response.body.error_description).toBe('image cant be uploaded!');
+  });
+
+  test('Test if /confirm works correctly with the correct data', async () => {
+    stubImageInterpret({ ok: true, payload: { value: 25 } })
+    stubImageUpload({ ok: true, payload: { url: 'http://test.com' } });
+
+    const postResponse = await createMeasure('2256', 'WATER', new Date());
+
+    const confirmData = {
+      measure_uuid: postResponse.body.measure_uuid,
+      confirmed_value: 200
+    };
+
+    const response = await request(app)
+      .patch('/confirm')
+      .send(confirmData)
+      .set('Accept', 'application/json');
+
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty('success');
+    expect(response.body.success).toBe(true);
   });
 });
